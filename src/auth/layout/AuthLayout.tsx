b3329e91693f6e99932f11f8b72007e496c22e44
Nini <raychu701@gmail.com>
2023-03-07 11:34:39 -0500

    register casi listo

--- src/auth/layout/AuthLayout.tsx
+++ src/auth/layout/AuthLayout.tsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import { Button } from '@mui/material';
+import styles from '../styles/auth.module.css';
+import PaletteIcon from '@material-ui/icons/Palette';
+import { useRef } from 'react';
+import { display } from '@mui/system';
 
-export const AuthLayout = () => {
+interface Props {
+  title?: string;
+  children: JSX.Element | JSX.Element[];
+}
+
+
+export const AuthLayout = ({ title, children }: Props ) => {
+
+  const inputColor = useRef<HTMLInputElement>(null)
+
+  const handleInputColor = () => {
+    if (inputColor.current) {
+      inputColor.current.click();
+    }
+  }
+
   return (
-    <div>AuthLayout</div>
+    <>
+    
+      <div className={styles['auth-container-layout']}>
+        
+          <div className={styles['auth-container-layout-div']}>
+
+              <h1>{ title }</h1>
+              
+              { children }
+              
+          </div>
+
+
+        <div style={{
+          position: "absolute",
+          top: 10,
+          right: 10,
+          display: 'flex',
+          flexDirection: 'column'
+        }}>
+
+          <Button
+            variant="contained"
+            color='inherit'
+            startIcon={<PaletteIcon /> }
+            onClick={handleInputColor}
+          />
+          <input 
+            ref={ inputColor } 
+            type="color" 
+            style={{ 
+              opacity: 0, 
+              visibility: "hidden",
+            }}
+          />
+          
+        </div>
+
+      </div>
+
+
+    
+    </>
   )
 }
--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -1,86 +1,115 @@
 import { Formik, Form } from "formik"
-import { MytextInput } from "../components/MyTextInput";
+// import { MytextInput } from "../components/MyTextInput";
 import * as Yup from 'yup';
+import { AuthLayout } from "../layout/AuthLayout";
+import { TextField, Button } from "@mui/material";
 
 export const RegisterPage = () => {
   return (
-    <div>
+    // title="Registrate para entrar a la cueva del grasoso"
+    <AuthLayout >  
 
-
-    <div>
-
-        <h1>Hola</h1>
-        
         <Formik
-            initialValues={{
-                user: '',
-                email: '',
-                password1: '',
-                password2: ''
-            }}
-            onSubmit={ (values) => {
-                console.log(values);
-                
-            }}
-            validationSchema={ Yup.object({
-                user: Yup.string()
-                    .required('required')
-                    .max( 10, 'must have a maximum of 10 characters' )
-                    .min( 2, 'must have a minimum of 2 characters'),
-                email: Yup.string()
-                    .required('required')
-                    .email(),
-                password1: Yup.string()
-                    .required('required')
-                    .min( 6, 'must have a minimum of 6 characters' ),
-                password2: Yup.string()
-                    .required('required')
-                    .oneOf([ Yup.ref('password1')], 'passwords must be the same')
-
-            })}
-
-            //* Validacion lista Mañana toca hacerlo bonito
-            
-        >
-
-            { () => (
-                <Form noValidate>
+                initialValues={{
+                    user: '',
+                    email: '',
+                    password1: '',
+                    password2: ''
+                }}
+                onSubmit={ (values) => {
+                    console.log(values);
                     
-                    <MytextInput 
-                        label="User" 
-                        name="user" 
-                        placeholder="user" 
-                        type="text" />
+                    
+                }}
+                validationSchema={ Yup.object({
+                    user: Yup.string()
+                        .required('required')
+                        .max( 10, 'must have a maximum of 10 characters' )
+                        .min( 2, 'must have a minimum of 2 characters'),
+                    email: Yup.string()
+                        .required('required')
+                        .email(),
+                    password1: Yup.string()
+                        .required('required')
+                        .min( 6, 'must have a minimum of 6 characters' ),
+                    password2: Yup.string()
+                        .required('required')
+                        .oneOf([ Yup.ref('password1')], 'passwords must be the same')
 
-                    <MytextInput 
-                        label="Email" 
-                        name="email" 
-                        placeholder="email" 
-                        type="email" />
+                })}                
+            >
 
-                    <MytextInput 
-                        label="Password" 
-                        name="password1" 
-                        placeholder="password" 
-                        type="password" />
+                { (formik) => (
+                    <Form style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap:"5px"
+                    }} >
 
-                    <MytextInput 
-                        label="Repeat password" 
-                        name="password2" 
-                        placeholder="repeat password" 
-                        type="password" />
+                        <TextField 
+                          name="user"
+                          label="user"
+                          value={formik.values.user}
+                          onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                          error={formik.touched.user && Boolean(formik.errors.user)}
+                          helperText={formik.touched.user && formik.errors.user}
+                          variant="standard" 
+                        />
 
+                        <TextField 
+                          name="email"
+                          label="email"
+                          value={formik.values.email}
+                          onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                          error={formik.touched.email && Boolean(formik.errors.email)}
+                          helperText={formik.touched.email && formik.errors.email}
+                          variant="standard" 
+                        />
+                        
+                        <TextField 
+                            type='password'
+                            name="password1"
+                            label="password"
+                            value={formik.values.password1}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.password1 && Boolean(formik.errors.password1)}
+                            helperText={formik.touched.password1 && formik.errors.password1}
+                            variant="standard" 
+                        />
 
-                    <button type="submit">Crear</button>
+                        <TextField 
+                            type='password'
+                            name="password2"
+                            label="repeat password"
+                            value={formik.values.password2}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.password2 && Boolean(formik.errors.password2)}
+                            helperText={formik.touched.password2 && formik.errors.password2}
+                            variant="standard" 
+                        />
+                      
 
-                </Form>
-            )}
+                    <Button 
+                        color="primary" 
+                        variant="contained" 
+                        fullWidth
+                        type="submit"
+                        sx={{
+                            marginTop: "10px"
+                        }} 
+                    >
+                        Submit
+                    </Button>
 
-        </Formik>
+                    </Form>
+                )}
 
-    </div>
+            </Formik>
 
-
-</div>
+    </AuthLayout>
   )
 }
